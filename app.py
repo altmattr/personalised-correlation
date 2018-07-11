a27828c1c813e6cf4ede789531413cdb7066c6ee
Emma Walker <emmmarosewalker@gmail.com>
2018-07-11 11:06:16 +1000

    add read csv functionality

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import csv
 app = Flask(__name__) 
 
+data = []
 
+with open('corr-matrix.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        data.append(row)
+
+
+# route for index
 @app.route('/')
 def hello():
-    return render_template("index.html")
-
+    return render_template("index.html", data=data)
 if __name__ == '__main__':
     app.run()
