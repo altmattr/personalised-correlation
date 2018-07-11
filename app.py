032e7ac5ece05313fc48ea19ba5eb9ecf6c8b331
Emma Walker <emmmarosewalker@gmail.com>
2018-07-11 16:34:51 +1000

    new matrix data

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ app = Flask(__name__)
 
 data = []
 
+symptomData = []
+
 with open('corr-matrix.csv', 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         data.append(row)
 
+with open('freq.csv', 'rt') as freqfile:
+    filereader = csv.reader(freqfile, delimiter=',',  quotechar='|')
+    for row in filereader:
+        symptomData.append(row)
+
 
 # route for index
 @app.route('/')
 def hello():
-    return render_template("index.html", data=data)
+    return render_template("index.html", data=data, symptomData=symptomData)
 if __name__ == '__main__':
     app.run()
