be9ddb1d5aeec26f552a62584a0d755eed5a098d
Emma Walker <emmmarosewalker@gmail.com>
2018-07-12 14:02:27 +1000

    scaled correlation matrix and updates to front end

--- app.py
+++ app.py
@@ -1,26 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 import os.path
+import dataProcessing
+from qualitricsApiDownloadsDATA import get_all_results
 app = Flask(__name__) 
 
-data = []
+def correlation_data_to_list():
+    data = []
+    with open('data/scaled-corr-matrix.csv', 'rt') as csvfile:
+        corr_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in corr_reader:
+            data.append(row)
+    return data
 
-symptomData = []
+def freq_data_to_list():
+    symptom_data = []
 
-with open('data/corr-matrix.csv', 'rt') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        data.append(row)
-
-with open('data/freq.csv', 'rt') as freqfile:
-    filereader = csv.reader(freqfile, delimiter=',',  quotechar='|')
-    for row in filereader:
-        symptomData.append(row)
+    with open('data/freq.csv', 'rt') as freqfile:
+        filereader = csv.reader(freqfile, delimiter=',',  quotechar='|')
+        for row in filereader:
+            symptom_data.append(row)
+    
+    return symptom_data
 
 
 # route for index
 @app.route('/')
 def hello():
-    return render_template("index.html", data=data, symptomData=symptomData)
+    data = correlation_data_to_list()
+    symptom_data = freq_data_to_list()
+    return render_template("index.html", data=data, symptomData=symptom_data)
+
+
+@app.route('/data')
+def get_survey_data():
+    # make a call to the qualtrics API to update the user data files
+    get_all_results(apiToken='CAd56GmSu02n04L1INwMYGOdaDzJXktCXiSGnUFJ', surveyId='SV_2i51uu8Vidq2zC5')
+
+    # run data.py to update data based on new information
+    dataProcessing.main()
+
+    # get the value of userID query string (i.e. ?userID=some-value)
+    userID = request.args.get('userID')
+
+    # get all the survey response data
+    survey_responses = []
+    with open ('data/DSM MQ Data Survey 1.2.csv', 'rt') as responsescsv:
+        reader = csv.reader(responsescsv, delimiter=',', quotechar='|')
+        for row in reader:
+            survey_responses.append(row)
+
+    # find the individual response data based on query string
+    i = len(survey_responses)-1
+    id_index = -1
+    while i >= 0:
+        # find the respondent's row
+        if survey_responses[i][0] == userID:
+            id_index = i
+            print(survey_responses[i])
+            # get results
+        i -= 1
+
+    return 'OK'
+
+
 if __name__ == '__main__':
     app.run()
+
--- dataProcessing.py
+++ dataProcessing.py
@@ -1,3 +1,4 @@
+
 # coding: utf-8
 import pandas as pd
 import csv
@@ -48,6 +49,7 @@ def create_freq_stats_per_avg(data):
 	Q2|2.5
 	Q3|2.2
 	"""
+	i_response = data.iloc[-1].fillna(0)
 	questions = {}
 	for col in data.columns:
 	    # score, count for this col
@@ -61,27 +63,31 @@ def create_freq_stats_per_avg(data):
 	    for k,v in values.items():
 	        total+= v
 	        count+=int(k)*v
+	    # add average response for question, and individual response for that question
+	    questions[col] = ['%.2f' % (count/total), int(i_response[col])]
 
-	    questions[col] = '%.2f' % (count/total)
+	ser = pd.Series(*questions.values())
+	ser.to_csv('data/freq.csv', header=True)
 
-	ser = pd.Series(questions) \
-	    .rename('Average')
-	ser.to_csv('data/freq.csv', index_label='Question', header=True)
-
-
-
-def main(create_stats=True, verbose=True):
+			
+			
+def main(create_stats=False, verbose=True):
 	if verbose: print('begun main processing')
 
 	# read in data, most resource intensive operation
-	data = pd.read_csv('data/qualtrics.csv', usecols=lambda x: x.startswith("Q"))
-	data.drop([0,1,5537],inplace=True)
+	data = pd.read_csv('data/qualtrics.csv')
+	# only get test values
+	data = data[data.DistributionChannel == 'test']
+	# drop null columns and non-question columns
 	drop_cols = [col for col, val in data.isnull().sum().items() if val > 5000]
+	# drop questions with invalid data
 	drop_cols.extend(['Q31','Q32'])
 	data.drop(drop_cols, axis=1,inplace=True)
 
-	if verbose: print('trimmed csv loaded')
+	data = data[[col for col in data.columns if col.startswith("Q")]]
 
+	if verbose: print('trimmed csv loaded')
+	
 	# create frequency statistics
 	if create_stats:
 		# CHANGE THIS to create_freq_stats_per_value if needed
@@ -90,20 +96,42 @@ def main(create_stats=True, verbose=True):
 		if verbose: print('freq stats created.')
 
 	# replace nulls with 0 for corr matrix
-	data.fillna(0,inplace=True)
+	#data.fillna(0,inplace=True)
+
 
 	# cast values to int
-	data = data.astype(int)
-
-	# create corr corr-matrix
+	data = data.astype(float)
+	
+	# create corr corr-matrix 
 	data_corr = data.corr()
 
-	# replacing negative correlations with zeroes
-	data_corr[data_corr < 0] = 0
+	# replacing negative correlations with  (not needed)
+	#data_corr[data_corr < 0] = 0
+
+	# find maxmimum value in whole matrix
+	# costly way in terms of processing
+	desc_max = data_corr[data_corr < 1].max().describe()
+	desc_min = data_corr[data_corr < 1].min().describe()
+	max_val = max(desc_max['max'],abs(desc_min['min']))
+	# rescales correlation matrix so that the old max value is now 1
+	scaled_corr = data_corr / max_val
+	scaled_corr[scaled_corr > 1] = 1
+
+	# turn into integer representation
+	scaled_corr = scaled_corr.round(1)*10
+	scaled_corr.astype(int, inplace=True)
+
 
 	# save corr matrix
 	data_corr.to_csv('data/corr-matrix.csv')
+	scaled_corr.to_csv('data/scaled-corr-matrix.csv')
 	if verbose: print('saved corr-matrix')
 
 if __name__ == '__main__':
+	import time
+	start = time.time()
+
 	main()
+
+	end = time.time()
+	print('main() process took ',end - start, ' seconds to execute.')
