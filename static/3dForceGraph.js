e54bd3755e2f0d0840decfd29f1ae754c39b09e2
TamDu <tam.du2@gmail.com>
2018-07-29 18:31:11 +1000

    Comments for 3D-graph

--- static/3dForceGraph.js
+++ static/3dForceGraph.js
@@ -1,3 +1,15 @@
+/* 
+ * Notes:
+ * 3d-graph library is currently being used, if this project is   
+ * to continue switch to D3.js. D3.js extends the current 3d-graph 
+ * and is more versatile.
+ * 
+ * This script is inefficent, it currently instantiating a new 3d graph
+ * every time the graph changes. The graph should generate based on the 
+ * changes made.
+ *
+ */
+
 //----
 //Other variables:
 var freqIdxStatus = 2;
@@ -7,7 +19,7 @@ var linkMode = ["All", "Only Positive", "Only Negative"]
 let toggledNode = "";
 let gData = [];
 
-//Button functionality:
+//Rotates what type of links to be displayed.
 function alterLinks() {
 	if (linkIdxStatus < linkMode.length-1)
 		linkIdxStatus++;
@@ -18,7 +30,7 @@ function alterLinks() {
 }
 
 
-// Node
+// Generates a js object in the format usable by 3d-graph, to produce a node
 function createNodes(dataMatrix, symptomRatings){
 	var nodes = [];
 	for (var i = 1; i < dataMatrix.length; i++){
@@ -36,7 +48,7 @@ function createNodes(dataMatrix, symptomRatings){
 }
 
 
-
+// Generates a js object in the format usable by 3d-graph, to representa link
 function createLinks(dataMatrix){
 	var links = [];
 	for (var i = 1; i < dataMatrix.length; i++){
@@ -68,6 +80,8 @@ function createLinks(dataMatrix){
 	return links;
 }
 
+//Creates a 3d-graph and displays nodes as 2d Text sprites (they need colliders)
+//to allow for physics or selection to work.
 function create3DGraph(worded = false){
 	//Calls Create Node to generate array of node objects.
 	let nodes = createNodes(dataMatrix, symptomRatings);
@@ -83,17 +97,20 @@ function create3DGraph(worded = false){
 		generate3DGraph();
 }
 
+//Checks what value is placed in the input field on the website
 function checkThreshold(){
 	linkThreshold = document.getElementById("corrStrText").value;
 	linkThreshold = Math.max(Math.min(linkThreshold, linkMaxThresh), linkMinThresh);
 	document.getElementById("corrStrText").value = linkThreshold;
 }
 
+//Generates a 3d graph that is called by a button
 function generate3DGraphButton(worded = false){
 	checkThreshold();
 	create3DGraph(worded);
 }
 
+//Generates a 3d graph that uses 2d text sprites as nodes.
 function generateWord3DGraph(){
 	let elem = document.getElementById('3d-graph');
 	
@@ -121,6 +138,7 @@ function generateWord3DGraph(){
 		.graphData(gData);
 }
 
+//Generates a 3d-graph based on the value of gData set
 function generate3DGraph(){
 	let elem = document.getElementById('3d-graph');
 	
@@ -157,6 +175,8 @@ function toggleHighlightNode(node){
 	create3DGraph();
 }
 
+//BETA is used to switch between which type of data representation you want to 
+//dispaly the group results as.
 function switchFreqMode(){
 	if (freqIdxStatus < freqMod.length +1)
 		freqIdxStatus++;
--- static/config.js
+++ static/config.js
@@ -1,7 +1,5 @@
 //Config Data:
 
-//Variables i need from Oscar
-
 //Color of nodes, index as rating
 var severityChart = 
 [
@@ -11,30 +9,48 @@ var severityChart =
 	"rgb(8, 61, 135, 1)",
 ]
 
+//The modifier to determine the size of a node based off the
+//group average
 var nodeSizeModifier = 1;
+//The maxmimum and minimum size a node can be
 var maxNodeSize = 10;
 var minNodeSize = 0.5;
+//Beta determines the size of a text node.
 var nodeTextSize = 8;
 
-//Determines what links will be displayed 
-//i.e anything less than -linkThreshold and anything greater than linkThreshold
+//Determines what links will not be displayed 
+//i.e anything less than linkMinThresh and anything greater than 
+//    linkMaxThreshold
 var linkMaxThresh = 10;
 var linkMinThresh = 0;
+//The default value the correlation strength will display.
 var linkThreshold = 5;
+
+//
 var linkWidthModifier = 0.5;
+//Maximum and Minimum of width/size of all links
 var maxLinkWidth = 8;
 var minLinkWidth = 0.1;
+
+//The colour of the positive correlations
 var posCorrLinkColor = "green";
+//The colour of the negative correlations
 var negCorrLinkColor = "red";
 
 //Determine if a node is being selected, if true the current selected node will
 //Highlight all the links connected to it and remove all other links
 var nodeSelect = false;
+//The opacity of all links by default.
+//I.E. 0 is completely transparent and 1 is opaque.
 var linkOpacity = 0.2;
+//The opacity of the highlighted link (if a node is being selected)
 var linkHOpacity = 1;
 var currentOpacity = linkOpacity;
 
 var responsiveWidth = window.innerWidth * 0.7;
+//Determines the number of tris
+//I.E. Increase the number to improve the image quality of the node.
+//     Decrease it to reduce the quality and improve speed
 var nodeResolution = 20;
 var labelTextColor = "black";
 var backgroundColor = "white";
