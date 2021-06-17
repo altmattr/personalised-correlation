41690e7f3f4a8f44c34ac1eccd82e01293ddc21b
Matthew Roberts <matthew.roberts@mq.edu.au>
2021-06-17 13:41:43 +1000

    improvements to docker_dev to clean up after itself

--- docker_dev.sh
+++ docker_dev.sh
@@ -1,2 +1,5 @@
+docker stop pc_dev
 docker build --tag pc_dev:latest --file Dockerfile .
 docker run --rm --name pc_dev -d -v $(pwd):/app -p 5000:5000 pc_dev:latest
+docker exec -it pc_dev /bin/bash
+docker stop pc_dev
